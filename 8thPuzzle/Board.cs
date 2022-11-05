e070dd435ec0913e01f35e6e4d1f625189ecacf2
Damian Ivanov <37500930+damianivanov@users.noreply.github.com>
2022-11-05 22:56:55 +0200

    little refactoring

--- 8thPuzzle/Board.cs
+++ 8thPuzzle/Board.cs
@@ -12,17 +12,17 @@ struct Coordinates
 
     public sbyte X { get; set; }
     public sbyte Y { get; set; }
-    
+
 }
 class Board
 {
-    
+
     public int[,] _tiles;
 
     private Coordinates _currIndexOfZero;
     public int ManhattanDistance { get; set; }
     public int[,] GoalState { get; private set; }
-    public Stack<string> PathString { get; set; } = new Stack<string>();
+    public List<string> PathString { get; set; } = new List<string>();
     public Stack<Board> Path { get; set; } = new Stack<Board>();
     public int Size { get; }
     public int FinalIndexOfZero { get; private set; }
@@ -48,7 +48,6 @@ class Board
             Ida_star();
         }
     }
-
     private int InversionCount(int[] numbers)
     {
         int inversions = 0;
@@ -67,13 +66,12 @@ class Board
         int[] tiles = new int[Size * Size];
         int index = 0;
         for (int i = 0; i < Size; i++)
-        for (int j = 0; j < Size; j++)
-            tiles[index++] = _tiles[i, j];
+            for (int j = 0; j < Size; j++)
+                tiles[index++] = _tiles[i, j];
         // row of 0 + inversion count, ako e cheten size-a 
         if (Size % 2 == 0) return (InversionCount(tiles) + _currIndexOfZero.X) % 2 != 0;
         else return InversionCount(tiles) % 2 == 0;
     }
-
     private Dictionary<string, Board?> Neighbors()
     {
         return new Dictionary<string, Board?>()
@@ -86,21 +84,25 @@ class Board
             .OrderBy(b => b.Value!.ManhattanDistance)
             .ToDictionary(b => b.Key, b => b.Value);
     }
-
     private Board? Move(sbyte tilesX, sbyte tilesY)
     {
         try
         {
-            if(!InsideMatrix(_currIndexOfZero.X+tilesX,_currIndexOfZero.Y+tilesY)) return null;
+
+            var newX = _currIndexOfZero.X + tilesX;
+            var newY = _currIndexOfZero.Y + tilesY;
+
+            if (!InsideMatrix(newX, newY)) return null;
             var oldZeroIndex = this._currIndexOfZero;
             var board = new Board(Size, _tiles, FinalIndexOfZero, null, this._currIndexOfZero);
-            board.Swap(_currIndexOfZero, _currIndexOfZero.X + tilesX, _currIndexOfZero.Y + tilesY);
-            board._currIndexOfZero.X += tilesX;
-            board._currIndexOfZero.Y += tilesY;
+            board.Swap(_currIndexOfZero, newX, newY);
+
+            board._currIndexOfZero.X = (sbyte)newX;
+            board._currIndexOfZero.Y = (sbyte)newY;
 
             board.ManhattanDistance = this.ManhattanDistance;
             board.ManhattanDistance += board.ModifiedManhatanDistance(oldZeroIndex, board._currIndexOfZero);
-            
+
             // board.Manhattan();
             return board;
         }
@@ -115,16 +117,13 @@ class Board
         return newX >= 0 && newY >= 0 && newX < Size && newY < Size;
     }
 
-    private int ModifiedManhatanDistance(Coordinates oldZero, Coordinates newZero)
-    {
-        var goalIndex = FindIndexOfValueGoalState(_tiles[oldZero.X, oldZero.Y]);
-        var oldDistance = Distance(newZero, goalIndex.X, goalIndex.Y);
-        var newDistance = Distance(oldZero, goalIndex.X, goalIndex.Y);
-        return newDistance - oldDistance;
-    }
+
 
     private void Swap(Coordinates coordinates, int newX, int newY)
     {
+        //var v = _tiles[coordinates.X, coordinates.Y];
+        //_tiles[coordinates.X, coordinates.Y] = _tiles[newX, newY];
+        //_tiles[newX,newY]=v; 
         (_tiles[coordinates.X, coordinates.Y], _tiles[newX, newY]) =
             (_tiles[newX, newY], _tiles[coordinates.X, coordinates.Y]);
     }
@@ -137,33 +136,35 @@ class Board
         visited.Add(this.ToString());
         while (Path.Count > 0)
         {
-            int t = Search(Path, 0, bound,visited);
+            int t = Search(Path, 0, bound, visited);
             if (t == 0) return;
             bound = t;
         }
     }
 
-    private int Search(Stack<Board> path, int currentCost, int bound,List<string>visited)
+    private int Search(Stack<Board> path, int currentCost, int bound, List<string> visited)
     {
         var currNode = path.Peek();
         int f = currentCost + currNode.ManhattanDistance;
         if (f > bound) return f;
-        if (currNode.ManhattanDistance==0 && currNode.IsGoal(this.GoalState)) return 0;
+        if (currNode.ManhattanDistance == 0 && currNode.IsGoal(this.GoalState)) return 0;
         int min = int.MaxValue;
         var neighbors = currNode.Neighbors();
         foreach (var node in neighbors)
         {
-            var str = node.Value.ToString();
-            if(visited.Contains(str)) continue;
-            PathString.Push(node.Key);
+            var str = node.Value!.ToString();
+            if (visited.Contains(str)) continue;
+            PathString.Add(node.Key);
             path.Push(node.Value!);
             visited.Add(str);
-            int t = Search(path, currentCost+1, bound,visited);
+
+            int t = Search(path, currentCost + 1, bound, visited);
             if (t == 0) return 0;
-            if (t <= min) min = t;
-            PathString.Pop();
+            if (t < min) min = t;
+
+            PathString.RemoveAt(PathString.Count - 1);
             path.Pop();
-            visited.RemoveAt(visited.Count-1);
+            visited.RemoveAt(visited.Count - 1);
         }
 
         return min;
@@ -171,11 +172,11 @@ class Board
     private Coordinates FindIndexOfValue(int value, int[,] board)
     {
         for (sbyte i = 0; i < Size; i++)
-        for (sbyte j = 0; j < Size; j++)
-            if (board[i, j] == value)
-                return new Coordinates(i,j);
+            for (sbyte j = 0; j < Size; j++)
+                if (board[i, j] == value)
+                    return new Coordinates(i, j);
 
-        return new Coordinates(-1,-1);
+        return new Coordinates(-1, -1);
     } //O(n^2)
 
     private Coordinates FindIndexOfValueGoalState(int value)
@@ -184,27 +185,35 @@ class Board
             value++;
         sbyte i = Convert.ToSByte((value - 1) / Size);
         sbyte j = Convert.ToSByte((value - 1) % Size);
-        return new Coordinates(i,j);
+        return new Coordinates(i, j);
     } // O(1)
 
     private void Manhattan()
     {
         int manhattanDistance = 0;
         for (var i = 0; i < Size; i++)
-        for (var j = 0; j < Size; j++)
-            if (_tiles[i, j] != 0)
-                manhattanDistance += Distance(FindIndexOfValueGoalState(_tiles[i, j]), i, j);
+            for (var j = 0; j < Size; j++)
+                if (_tiles[i, j] != 0)
+                    manhattanDistance += Distance(FindIndexOfValueGoalState(_tiles[i, j]), i, j);
         this.ManhattanDistance = manhattanDistance;
     } //O(n^2)
 
-    private int Distance(Coordinates el1, int i, int j) => Math.Abs(el1.X - i) + Math.Abs(el1.Y - j);
+    private int ModifiedManhatanDistance(Coordinates oldZero, Coordinates newZero)
+    {
+        var goalIndex = FindIndexOfValueGoalState(_tiles[oldZero.X, oldZero.Y]);
+        var oldDistance = Distance(newZero, goalIndex.X, goalIndex.Y);
+        var newDistance = Distance(oldZero, goalIndex.X, goalIndex.Y);
+        return newDistance - oldDistance;
+    }//O(1)
+
+    private int Distance(Coordinates el1, int i, int j) { return Math.Abs(el1.X - i) + Math.Abs(el1.Y - j); }
 
     private bool IsGoal(int[,] goalState)
     {
         for (var i = 0; i < Size; i++)
-        for (int j = 0; j < Size; j++)
-            if (this._tiles[i, j] != goalState[i, j])
-                return false;
+            for (int j = 0; j < Size; j++)
+                if (this._tiles[i, j] != goalState[i, j])
+                    return false;
         return true;
     }
 
@@ -227,14 +236,11 @@ class Board
     {
         return Move(0, 1);
     }
-    
-
-    
 
     public override string ToString()
     {
         string str = "";
-        for (int i = 0; i <Size; i++)
+        for (int i = 0; i < Size; i++)
         {
             for (int j = 0; j < Size; j++)
             {
@@ -243,13 +249,5 @@ class Board
         }
 
         return str;
-    } 
-
-    private bool Equals(Board? other)
-    {
-        if (ReferenceEquals(null, other)) return false;
-        if (ReferenceEquals(this, other)) return true;
-        return ManhattanDistance == other.ManhattanDistance && 
-               _tiles.Equals(other._tiles) && Size == other.Size;
     }
 }
